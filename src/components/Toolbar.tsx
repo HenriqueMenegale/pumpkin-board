e7d0bf618512f5be3ca4fa3d8c95c5a6ab40d114
henriquemenegale <hello@henriquemenegale.com>
2025-12-14 17:50:18 -0300

    update toolbar ui

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,3 +1,5 @@
+import { FiPlay, FiPause, FiImage, FiVideo, FiTool } from 'react-icons/fi';
+
 type ToolbarProps = {
   onPlayAll: () => void;
   onPauseAll: () => void;
@@ -16,12 +18,52 @@ export function Toolbar({
 }: ToolbarProps) {
   return (
     <div className="wb-controls">
-      <button onClick={onPlayAll} className="btn">Play all videos</button>
-      <button onClick={onPauseAll} className="btn">Pause all videos</button>
-      <button onClick={onAddImage} className="btn btn-blue">Add image via URL</button>
-      <button onClick={onAddVideo} className="btn btn-blue">Add video via URL</button>
+      <button
+        onClick={onPlayAll}
+        className="btn btn-icon"
+        aria-label="Play all videos"
+        title="Play all videos"
+      >
+        <FiPlay className="icon" />
+        <span className="icon-text">Play all</span>
+      </button>
+      <button
+        onClick={onPauseAll}
+        className="btn btn-icon"
+        aria-label="Pause all videos"
+        title="Pause all videos"
+      >
+        <FiPause className="icon" />
+        <span className="icon-text">Pause all</span>
+      </button>
+      <button
+        onClick={onAddImage}
+        className="btn btn-blue btn-icon"
+        aria-label="Add image via URL"
+        title="Add image via URL"
+      >
+        <FiImage className="icon" />
+        <span className="icon-text">Add image</span>
+      </button>
+      <button
+        onClick={onAddVideo}
+        className="btn btn-blue btn-icon"
+        aria-label="Add video via URL"
+        title="Add video via URL"
+      >
+        <FiVideo className="icon" />
+        <span className="icon-text">Add video</span>
+      </button>
       {onToggleDebug && (
-        <button onClick={onToggleDebug} className="btn">Debug</button>
+        <button
+          onClick={onToggleDebug}
+          className="btn btn-icon"
+          aria-label="Debug menu"
+          title="Debug menu"
+        >
+          <FiTool className="icon" />
+          <span className="icon-text">Debug</span>
+        </button>
       )}
     </div>
   );
